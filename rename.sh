718bfc5976fe087906612ff688d79d821df777f0
mohammed jaseem <147907994+mohammed-Jaseem1@users.noreply.github.com>
2025-05-19 15:49:08 +0530

    Add files via upload

---
+++ rename.sh
@@ -0,0 +1,19 @@
+ set -vx
+if [ $# -gt 1 ]
+then 
+echo "\n Synatx is <$0><location> or <$0>"
+exit 1
+fi  
+if [ $# -ne 0 ]
+then 
+cd $1
+fi 
+for i in *
+do 
+if [ -f $i ]
+then 
+mv $i $i.$$
+fi 
+done
+echo<"filename changed to filename..$$">
+
